d58107a988a8923eca71cad1f45bbb83a3e15bc5
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-13 23:52:54 +0530

    Added third program

---
+++ 3prog.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function to check prime
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int x;
+    
+    printf("Enter a positive number: ");
+    scanf("%d", &x);
+
+    int smaller = x - 1;
+    int greater = x + 1;
+
+    // Find nearest smaller prime
+    while (smaller > 1) {
+        if (isPrime(smaller)) {
+            break;
+        }
+        smaller--;
+    }
+
+    // Find nearest greater prime
+    while (1) {
+        if (isPrime(greater)) {
+            break;
+        }
+        greater++;
+    }
+
+    // Output
+    if (smaller > 1)
+        printf("\nA prime number less than %d is %d.\n", x, smaller);
+    else
+        printf("\nNo prime number exists less than %d.\n", x);
+
+    printf("A prime number greater than %d is %d.\n", x, greater);
+
+    return 0;
+}
