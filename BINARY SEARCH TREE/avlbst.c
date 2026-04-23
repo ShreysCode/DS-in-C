8812e792f7b81293d5bf50b741ece8c8dd723b33
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-23 23:56:43 +0530

    Node creation

--- BINARY SEARCH TREE/avlbst.c
+++ BINARY SEARCH TREE/avlbst.c
@@ -5,3 +5,17 @@ struct Node {
     int data, height;
     struct Node *left, *right;
 };
+
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int height(struct Node* n) {
+    return n ? n->height : 0;
+}
+
+struct Node* createNode(int val) {
+    struct Node* n = malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    n->height = 1;
+    return n;
+}
