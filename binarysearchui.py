76dbc075bb553337d83f4932224b49ebc7dbf6a1
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-23 22:16:02 +0530

    Added a BST program with user input

---
+++ binarysearchui.py
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
+}
+
+struct Node* insert(struct Node* root, int val) {
+    if (root == NULL) return createNode(val);
+
+    if (val < root->data)
+        root->left = insert(root->left, val);
+    else if (val > root->data)
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if (root) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct Node* root = NULL;
+    int n, val;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter value: ");
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    printf("Inorder traversal: ");
+    inorder(root);
+
+    return 0;
+}
