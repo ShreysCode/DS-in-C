3ba356a5fde28ceee5f997aea2e6185e2754f352
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-23 23:58:37 +0530

    Node Insertion

--- BINARY SEARCH TREE/avlbst.c
+++ BINARY SEARCH TREE/avlbst.c
@@ -51,3 +51,40 @@ struct Node* leftRotate(struct Node* x) {
 int getBalance(struct Node* n) {
     return n ? height(n->left) - height(n->right) : 0;
 }
+
+struct Node* insert(struct Node* node, int key) {
+    if (!node) return createNode(key);
+
+    if (key < node->data)
+        node->left = insert(node->left, key);
+    else if (key > node->data)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = 1 + max(height(node->left), height(node->right));
+
+    int balance = getBalance(node);
+
+    // LL
+    if (balance > 1 && key < node->left->data)
+        return rightRotate(node);
+
+    // RR
+    if (balance < -1 && key > node->right->data)
+        return leftRotate(node);
+
+    // LR
+    if (balance > 1 && key > node->left->data) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // RL
+    if (balance < -1 && key < node->right->data) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
