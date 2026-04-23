07d362ce690bd9610e21b0211a7935175c156ce2
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-23 22:02:55 +0530

    Structure Node creation

--- binarysearch.c
+++ binarysearch.c
@@ -7,3 +7,11 @@ struct Node {
     struct Node* left;
     struct Node* right;
 };
+
+// Create new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
