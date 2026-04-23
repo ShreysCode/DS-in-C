4ffe80b6a23f3acc912f94dcafbc64c55b6d901d
ShreysCode <cshreyyyforadbu@gmail.com>
2026-04-23 22:04:13 +0530

    Search Node

--- binarysearch.c
+++ binarysearch.c
@@ -28,3 +28,14 @@ struct Node* insert(struct Node* root, int value) {
 
     return root;
 }
+
+// Search node
+struct Node* search(struct Node* root, int key) {
+    if (root == NULL || root->data == key)
+        return root;
+
+    if (key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
