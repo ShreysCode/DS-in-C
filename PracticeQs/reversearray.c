89b6b42d44844a121f7fb0f384739b44bb1d9eba
ShreysCode <cshreyyyforadbu@gmail.com>
2026-05-10 22:19:17 +0530

    Reversed array without another array

---
+++ PracticeQs/reversearray.c
@@ -0,0 +1,28 @@
+//Reverse an array without using another array.
+
+#include <stdio.h>
+
+void reverse(int arr[], int n) {
+    int start = 0, end = n - 1;
+
+    while(start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1,2,3,4,5};
+    int n = 5;
+
+    reverse(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
