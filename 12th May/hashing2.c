93a5c005c9b00c4ac1e4332c4b4de9b7ea583480
ShreysCode <cshreyyyforadbu@gmail.com>
2026-05-12 11:02:54 +0530

    Second hashing program

---
+++ 12th May/hashing2.c
@@ -0,0 +1,5 @@
+// Write a C program to store the following elements in an array by hashing. Consider hash function as x%11. 
+// Use linear probing in case of collision.
+//  26, 99, 55, 91, 50, 32, 77, 44, 66, 29 
+// Display the contents of the array after storing the elements. 
+
